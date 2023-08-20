b849e009a92df269627e5482597ef04813a6762b
matthewelmer <matthewdavidelmer@gmail.com>
2023-08-19 23:36:51 -0500

    add failing unit test for search function

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,21 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let contents = fs::read_to_string(config.file_path)?;
 
-  println!("With text:\n{contents}");
-
   Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick two.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+  }
 }
